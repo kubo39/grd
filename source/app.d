9497c21c6fed817b0c881edb85da16540778584f
Hiroki Noda <kubo39@gmail.com>
2019-02-28 17:20:18 +0900

    Parsing command line arguments with std.getopt

--- source/app.d
+++ source/app.d
@@ -1,6 +1,31 @@
+import std.getopt;
 import std.stdio;
 
-void main()
+void usage()
 {
-	writeln("Edit source/app.d to start your project.");
+    stderr.writeln(`
+USAGE:
+    grd <pattern> <path>
+`);
+}
+
+int main(string[] args)
+{
+    string output = void;
+    auto helpInformation = args.getopt("o|output", &output);
+    if (helpInformation.helpWanted)
+    {
+        usage();
+        return 0;
+    }
+    if (args.length < 2)
+    {
+        usage();
+        return 1;
+    }
+    string pattern = args[1];
+    string path = args[2];
+
+    writeln(args[1 .. 3]);
+    return 0;
 }
